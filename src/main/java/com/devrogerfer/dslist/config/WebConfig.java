52f7d6b4643d96aaeee1c3df2d3e745c24065e75
Rogerio Fernandes <roferrock@gmail.com>
2025-09-25 21:28:38 -0300

    Approval environment ok

--- src/main/java/com/devrogerfer/dslist/config/WebConfig.java
+++ src/main/java/com/devrogerfer/dslist/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.devrogerfer.dslist.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,11 +13,11 @@ public class WebConfig {
 	@Value("${cors.origins}")
 	private String corsOrigins;
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
 			}
 		};
